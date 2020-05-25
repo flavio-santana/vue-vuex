0efb1b58d0db4e64cf93fca92b70c5b4f3b52139
Flavio Santana <f.o.santana@gmail.com>
2020-05-25 11:57:51 -0300

    Implentado utilização de função auxiliar MapState

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -3,6 +3,14 @@
         <h2 class="font-weight-light">
             Contador : {{ contador }}
         </h2>
+
+        <h4 class="font-weight-light">
+            Contador : {{ contadorAlias }}
+        </h4>
+
+         <h4 class="font-weight-light">
+            Contador Multiplicado : {{ contadorMultiplicado }}
+        </h4>
         
         <button 
             class="class-btn-lg btn-danger"
@@ -19,17 +27,26 @@
     </div>
 </template>
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
-    /*data(){
+    data(){
         return {
-            contador: 0
-        }
-    },*/
-    computed:{
-        contador(){
-            return this.$store.state.contador 
+            contadorLocal : 10
         }
     },
+    /**
+     * Mapeando propries para serem utilizadas no components 
+     */
+    //computed: mapState(['contador']),
+    computed: mapState({
+        contador: 'contador',
+        contadorAlias: 'contador',
+        contadorMultiplicado(state){
+            return state.contador * this.contadorLocal
+        }
+    }),
     methods:{
         decrementar(){
             this.$store.state.contador --
